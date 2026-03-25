bbb45fccf1ef80fe59196e621d3653b82841dffa
Jayson Knight <jayson.knight@jaysonknight.com>
2026-03-24 21:17:09 -0400

    Update JK.com AdGuard rules version and metadata
    Updated version and metadata for JK.com AdGuard rules.
    
    Signed-off-by: Jayson Knight <jayson.knight@jaysonknight.com>

---
+++ src/models/index.ts
@@ -0,0 +1,28 @@
+/**
+ * AdGuard DNS API Models
+ * TypeScript types matching OpenAPI spec v1.11
+ */
+
+// Enums
+export * from './enums.ts';
+
+// Account models
+export * from './account.ts';
+
+// Authentication models
+export * from './auth.ts';
+
+// Device models
+export * from './device.ts';
+
+// DNS Server models
+export * from './dns-server.ts';
+
+// Statistics models
+export * from './statistics.ts';
+
+// Query log models
+export * from './query-log.ts';
+
+// Error models
+export * from './errors.ts';
